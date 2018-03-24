787d8024437cbb96d46d8d2e4045cb3be02a0446
litian <litiandlut@163.com>
2018-03-24 18:29:33 +0800

    add sth.

--- flask/ctx.py
+++ flask/ctx.py
@@ -1,5 +1,5 @@
 '''Created on 2018年3月23日@author: litian'''
-
+import sys
 
 
 class RequestContext(object):
@@ -28,3 +28,41 @@ class RequestContext(object):
             self.request.url_rule = url_rule
         except HTTPException as e:
             self.request.routing_exception = e
+            
+    def push(self):
+        """Binds the request context to the current context."""
+        # If an exception occurs in debug mode or if context preservation is
+        # activated under exception situations exactly one context stays
+        # on the stack.  The rationale is that you want to access that
+        # information under debug situations.  However if someone forgets to
+        # pop that context again we want to make sure that on the next push
+        # it's invalidated, otherwise we run at risk that something leaks
+        # memory.  This is usually only a problem in test suite since this
+        # functionality is not active in production environments.
+        top = _request_ctx_stack.top
+        if top is not None and top.preserved:
+            top.pop(top._preserved_exc)
+
+        # Before we push the request context we have to ensure that there
+        # is an application context.
+        app_ctx = _app_ctx_stack.top
+        if app_ctx is None or app_ctx.app != self.app:
+            app_ctx = self.app.app_context()
+            app_ctx.push()
+            self._implicit_app_ctx_stack.append(app_ctx)
+        else:
+            self._implicit_app_ctx_stack.append(None)
+
+        if hasattr(sys, 'exc_clear'):
+            sys.exc_clear()
+
+        _request_ctx_stack.push(self)
+
+        # Open the session at the moment that the request context is
+        # available. This allows a custom open_session method to use the
+        # request context (e.g. code that access database information
+        # stored on `g` instead of the appcontext).
+        self.session = self.app.open_session(self.request)
+        if self.session is None:
+            self.session = self.app.make_null_session()        
+    
---
+++ flask_old_version/werkzeug01/local.py
@@ -0,0 +1,28 @@
+'''Created on 2018年3月24日@author: litian'''
+
+
+
+
+
+
+class LocalStack(object):
+
+    def __init__(self):
+        self._local = Local()
+
+    def push(self, obj):
+        rv = getattr(self._local, 'stack', None)
+        if rv is None:
+            self._local.stack = rv = []
+        rv.append(obj)
+        return rv
+
+    def pop(self):
+        stack = getattr(self._local, 'stack', None)
+        if stack is None:
+            return None
+        elif len(stack) == 1:
+            release_local(self._local)
+            return stack[-1]
+        else:
+            return stack.pop()
